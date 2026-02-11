6da72040ddc2b93c463fec39bf9bc0e9b7e9f7cc
Saeedeh Karimi <saeedehkarimi1994@gmail.com>
2026-02-11 21:14:46 +0100

    Updated

--- react-chatBot/src/components/sidebar/Sidebar.jsx
+++ react-chatBot/src/components/sidebar/Sidebar.jsx
@@ -1,8 +1,22 @@
 import styles from './Sidebar.module.css';
-export function Sidebar(){
+
+const CHATS = [
+    {id:1, title: "How to use AI"},
+    {id:2, title:"What is React?"},
+    {id:3, title:"What is JavaScript?"},
+    {id:4, title:"Gemini or ChatGPT?"},
+]
+
+
+
+export function Sidebar({chats=CHATS, activeChatId = 1}){
     return(
         <div className={styles.Sidebar}>
-            Sidebar
+            <ul>
+                {chats.map(chat=>
+                    <li key={chat.id}>{chat.title}</li>
+                )}
+            </ul>
         </div>
     )
 }
